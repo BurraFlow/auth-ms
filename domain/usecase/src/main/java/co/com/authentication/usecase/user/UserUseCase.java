4dc787fe9436a60483377f93dcad6a7301686464
Angel Espinosa <teamburra123@gmail.com>
2025-08-24 19:19:08 -0500

    add schema

--- domain/usecase/src/main/java/co/com/authentication/usecase/user/UserUseCase.java
+++ domain/usecase/src/main/java/co/com/authentication/usecase/user/UserUseCase.java
@@ -10,27 +10,21 @@ import reactor.core.publisher.Mono;
 public class UserUseCase {
 
   private final UserRepository userRepository;
-
   public Mono<User> saveUser(User user) {
     return userRepository.saveUser(user);
   }
-
   public Mono<User> findById(Long id) {
     return userRepository.findById(id);
   }
-
   public Mono<User> findByEmail(String email) {
     return userRepository.findByEmail(email);
   }
-
   public Flux<User> findAll() {
     return userRepository.findAll();
   }
-
   public Mono<User> updateUser(Long id, User user) {
     return userRepository.update(id, user);
   }
-
   public Mono<Void> deleteById(Long id) {
     return userRepository.deleteById(id);
   }
